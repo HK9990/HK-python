de4706a7ff0c7225c2d35d21d06c7c01771be52c
HK9990 <hussainkundli9990@gmail.com>
2024-02-28 14:30:22 +0530

    add python file

---
+++ python.py
@@ -0,0 +1,11 @@
+salary =  input("salary  per  day or month : ")
+
+if(salary == "day"):
+    day = int(input("salary/day : "))
+    tax_d = day*(0.05,0.25) [day > 5000]
+    print("tax :", tax_d)
+
+elif(salary == "month"):
+    month = int(input("salary/month : "))
+    tax_m = month*(0.05,0.25) [month > 50000]
+    print("tax :", tax_m)
